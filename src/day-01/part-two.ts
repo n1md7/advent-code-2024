6fb1502bc599a62185dace989c287ab666aed802
n1md7 <b.kodua@gmail.com>
2024-12-10 00:22:50 +0200

    solved: day 1, part 2

--- src/day-01/part-two.ts
+++ src/day-01/part-two.ts
@@ -1,5 +1,4 @@
-
-import { Solution } from '/src/interfaces/solution';
+import type { Solution } from '@src/interfaces/solution';
 
 export class PartTwo implements Solution<number> {
   private readonly input: string[];
@@ -9,7 +8,27 @@ export class PartTwo implements Solution<number> {
   }
 
   solve(): number {
-    return 0;
+    const one: number[] = [];
+    const two: number[] = [];
+
+    this.input.forEach(line => {
+      const [a, b] = line.split('   ').map(Number);
+      one.push(a);
+      two.push(b);
+    });
+
+    const counted = new Map<number, number>();
+
+    two.forEach(value => {
+      const count = counted.get(value) ?? 0;
+      counted.set(value, count + 1);
+    });
+
+    return one.reduce((sum, value) => {
+      const count = counted.get(value) ?? 0;
+      const score = value * count;
+
+      return sum + score;
+    }, 0);
   }
 }
-
--- tests/unit/day-01/part-two.test.ts
+++ tests/unit/day-01/part-two.test.ts
@@ -1,21 +1,27 @@
-
-import { PartTwo } from '/src/day-01/part-two';
+import { PartTwo } from '@src/day-01/part-two';
 import { expect, it, describe } from 'vitest';
 import { readFileSync } from 'fs';
 import { cwd } from 'node:process';
 
 const path = cwd() + '/src/day-01/input.txt';
 const input = readFileSync(path, 'utf-8');
-const example = `aa.bb.cc`;
+const example = `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+`;
 
-describe.skip('Day 01 - Part 2', () => {
+describe('Day 01 - Part 2', () => {
   it('should verify example input', () => {
     const payload = example.trim();
     const partOne = new PartTwo(payload);
 
     const result = partOne.solve();
 
-    expect(result).toBe(0);
+    expect(result).toBe(31);
   });
 
   it('should verify real input', () => {
@@ -24,7 +30,6 @@ describe.skip('Day 01 - Part 2', () => {
 
     const result = partOne.solve();
 
-    expect(result).toBe(100);
+    expect(result).toBe(22962826);
   });
 });
-
